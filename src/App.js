b0d0d503f2894f308688ad647b5b587648b14b86
Travis Libby <travis@robinpowered.com>
2017-11-13 19:34:57 -0500

    Refactor to have all books on stae.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ import { SHELVES } from './constants/shelves';
 
 class BooksApp extends React.Component {
   state = {
-    currentlyReading: [],
-    wantToRead: [],
-    read: []
+    books: []
   };
 
   /**
@@ -19,108 +17,13 @@ class BooksApp extends React.Component {
    * @param  {string} shelf The new shelf for the book.
    * @param  {Object} book  The book to move.
    */
-  moveBook = (shelf, book) => BooksAPI.update(book, shelf)
+  moveBook = (shelf, bookToUpdate) => BooksAPI.update(bookToUpdate, shelf)
     .then(() => {
-      this.removeFromShelf(book);
-      if (shelf !== SHELVES.none) {
-        this.addToShelf(shelf, book);
-      }
+      bookToUpdate.shelf = shelf;
+      this.setState(prevState => ({
+        books: prevState.books.filter(book => book.id !== bookToUpdate.id).concat(bookToUpdate)
+      }));
     });
-
-  /**
-   * Removes the given book from its current shelf.
-   * @param  {Object} book The book to remove from its shelf.
-   */
-  removeFromShelf = (book) => {
-    const {id} = book,
-          shelf = book.shelf || this.getShelf(book);
-
-    if (shelf === SHELVES.currentlyReading) {
-      this.removeFromCurrentlyReading(id);
-    } else if (shelf === SHELVES.wantToRead) {
-      this.removeFromWantToRead(id);
-    } else if (shelf === SHELVES.read) {
-      this.removeFromRead(id);
-    }
-  };
-
-  /**
-   * Adds the given book to the given shelf.
-   * @param {string} shelf The new shelf for the book.
-   * @param {Object} book  The book to add to the shelf.
-   */
-  addToShelf = (shelf, book) => {
-    if (shelf === SHELVES.currentlyReading) {
-      this.addToCurrentlyReading(book);
-    } else if (shelf === SHELVES.wantToRead) {
-      this.addToWantToRead(book);
-    } else if (shelf === SHELVES.read) {
-      this.addToRead(book);
-    }
-  }
-
-  /**
-   * Removes the book with the given id from the 'Currently Reading' shelf.
-   * @param  {number} id The id of the book to remove.
-   */
-  removeFromCurrentlyReading = (id) => {
-    this.setState((prevState) => ({
-      currentlyReading: prevState.currentlyReading.filter(book => book.id !== id)
-    }));
-  };
-
-  /**
-   * Adds the given book to the 'Currently Reading' shelf.
-   * @param {Object} book The book to add to the shelf.
-   */
-  addToCurrentlyReading = (book) => {
-    book.shelf = SHELVES.currentlyReading;
-    this.setState((prevState) => ({
-      currentlyReading: prevState.currentlyReading.concat([book])
-    }));
-  };
-
-  /**
-   * Removes the book with the given id from the 'Want to Read' shelf.
-   * @param {number} id The id of the book to remove.
-   */
-  removeFromWantToRead = (id) => {
-    this.setState((prevState) => ({
-      wantToRead: prevState.wantToRead.filter(book => book.id !== id)
-    }));
-  };
-
-  /**
-   * Adds the given book to the 'Want to Read' shelf.
-   * @param {Object} book The book to add to the shelf.
-   */
-  addToWantToRead = (book) => {
-    book.shelf = SHELVES.wantToRead;
-    this.setState((prevState) => ({
-      wantToRead: prevState.wantToRead.concat([book])
-    })); 
-  };
-
-  /**
-   * Removes the book with the given id from the 'Read' shelf.
-   * @param  {number} id The id of the book to remove.
-   */
-  removeFromRead = (id) => {
-    this.setState((prevState) => ({
-      read: this.state.read.filter(book => book.id !== id)
-    }));
-  };
-
-  /**
-   * Adds the given book to the 'Read' shelf.
-   * @param {Object} book The book to add to the shelf.
-   */
-  addToRead = (book) => {
-    book.shelf = SHELVES.read;
-    this.setState((prevState) => ({
-      read: prevState.read.concat([book])
-    }));
-  };
   
   /**
    * Gets the shelf that the book in the search results is currently sitting on.
@@ -128,39 +31,17 @@ class BooksApp extends React.Component {
    * @return {string}      The name of the shelf that the book is currently sitting on.
    */
   getShelf = (book) => {
-    const shelvedBooks = [
-      ...this.state.currentlyReading, 
-      ...this.state.wantToRead, 
-      ...this.state.read
-    ];
-    const foundBook = shelvedBooks.find(item => item.id === book.id);
+    const foundBook = this.state.books.find(item => item.id === book.id);
     return foundBook ? foundBook.shelf : SHELVES.none;
   };
   
-  
-
   componentDidMount() {
     let sortBooks;
 
     // Gets all the books and distributes them to the correct shelves.
     BooksAPI.getAll().then((books) => {
-      sortBooks(books);
+      this.setState({books});
     });
-
-    /**
-     * Distributes the given books to the correct shelves.
-     * @param  {<Array>.Object} books The collection of books to sort.
-     * @return {Object}               The updated state of the bookshelf.
-     */
-    sortBooks = (books) => {
-      this.setState(() => {
-        return {
-          currentlyReading: books.filter((book) => book.shelf === 'currentlyReading'),
-          wantToRead: books.filter((book) => book.shelf === 'wantToRead'),
-          read: books.filter((book) => book.shelf === 'read'),
-        };
-      });
-    };
   }
 
   render() {
@@ -170,7 +51,7 @@ class BooksApp extends React.Component {
       READ: 'Read'
     };
 
-    const {currentlyReading, wantToRead, read} = this.state;
+    const {books} = this.state;
 
     return (
       <div className="app">
@@ -182,9 +63,21 @@ class BooksApp extends React.Component {
             <div className="list-books">
               <Header />
               <div className="list-books-content">
-                <Bookshelf category={CATEGORIES.READING} books={currentlyReading} moveBook={this.moveBook} />
-                <Bookshelf category={CATEGORIES.WANT_TO_READ} books={wantToRead} moveBook={this.moveBook} />
-                <Bookshelf category={CATEGORIES.READ} books={read} moveBook={this.moveBook} />
+                <Bookshelf 
+                  category={CATEGORIES.READING} 
+                  books={books.filter(book => book.shelf === SHELVES.currentlyReading)} 
+                  moveBook={this.moveBook} 
+                />
+                <Bookshelf 
+                  category={CATEGORIES.WANT_TO_READ} 
+                  books={books.filter(book => book.shelf === SHELVES.wantToRead)} 
+                  moveBook={this.moveBook} 
+                />
+                <Bookshelf 
+                  category={CATEGORIES.READ} 
+                  books={books.filter(book => book.shelf === SHELVES.read)} 
+                  moveBook={this.moveBook} 
+                />
               </div>
               <div className="open-search">
                 <Link to="/search">Add a book</Link>
